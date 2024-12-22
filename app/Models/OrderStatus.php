51a11a3b9597e0570f51b43bf3de3357058f7567
Ardimas <dimasdwisuprayogi@gmail.com>
2024-12-22 13:22:06 +0700

    New OrderStatus model

---
+++ app/Models/OrderStatus.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderStatus extends Model
+{
+    use HasFactory;
+
+    protected $table = 'order_status';
+
+    protected $fillable = [
+        'uuid',
+        'status',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s'
+    ];
+
+    public function transactions()
+    {
+        return $this->hasOne(Transaction::class, 'uuid', 'uuid');
+    }
+}
