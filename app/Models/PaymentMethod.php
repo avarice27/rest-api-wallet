b95f8ca4e4c650d4c7dd37bb18c0254787d42b50
Ardimas <dimasdwisuprayogi@gmail.com>
2024-12-22 13:22:33 +0700

    Update PaymentMethod - Penambahan relasi ke model Transaction (baris 27)

--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -23,4 +23,9 @@ class PaymentMethod extends Model
         'created_at' => 'datetime:Y-m-d H:m:s',
         'updated_at' => 'datetime:Y-m-d H:m:s'
     ];
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
